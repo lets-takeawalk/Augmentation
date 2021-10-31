38efdf05aca0d28d664f0fb1f7f3e1a5e73c7f12
yeonheelee <mrncd1@gmail.com>
2021-10-31 19:37:42 +0900

    edit type issue

--- change_cls_num_1031.py
+++ change_cls_num_1031.py
@@ -1,17 +1,16 @@
 from glob import glob
-import os 
+import os
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 IMAGE_DIR=THIS_FOLDER+'/aug_1031_except6'
 file_list=os.listdir(IMAGE_DIR)
 for file in file_list:
-    if num(file)<6:continue
+    if int(file)<6:continue
     txt_list=glob(IMAGE_DIR+'/'+file+'/*.txt')
     for txt in txt_list:
         coordinates=''
         with open(txt,'r') as f:
             coordinates=f.read()
         coordinates=coordinates.split(' ')
-        coordinates[0]=coordinates[0]-1
+        coordinates[0]=str(int(coordinates[0])-1)
         with open(txt,'w') as f:
             f.write(' '.join(coordinates))
-        
