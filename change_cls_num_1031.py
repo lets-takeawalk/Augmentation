86d0e76c3b9b5c63adb609d616b0288b18fad6b1
yhLee11 <mrncd1@gmail.com>
2021-10-31 19:22:01 +0900

    change class number(except building 6)

---
+++ change_cls_num_1031.py
@@ -0,0 +1,17 @@
+from glob import glob
+import os 
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+IMAGE_DIR=THIS_FOLDER+'/aug_1031_except6'
+file_list=os.listdir(IMAGE_DIR)
+for file in file_list:
+    if num(file)<6:continue
+    txt_list=glob(IMAGE_DIR+'/'+file+'/*.txt')
+    for txt in txt_list:
+        coordinates=''
+        with open(txt,'r') as f:
+            coordinates=f.read()
+        coordinates=coordinates.split(' ')
+        coordinates[0]=coordinates[0]-1
+        with open(txt,'w') as f:
+            f.write(' '.join(coordinates))
+        
